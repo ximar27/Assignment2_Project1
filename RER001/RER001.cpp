7a91e28ee72eaac4df568f0099c6040ec8fb6aa8
ksima <ksima@SIMARJEET-KAUR>
2023-10-13 08:06:55 -0500

    length and width changing tests have been made

--- RER001/RER001.cpp
+++ RER001/RER001.cpp
@@ -3,6 +3,11 @@
 
 extern "C" int getPerimeter(int*length, int*width);
 extern "C" int getArea(int* length, int* width);
+extern "C" int setLength(int input, int* length);
+
+
+
+
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -29,5 +34,53 @@ namespace RER001
 			result = getArea(&length, &width);
 			Assert::AreEqual(20, result);
 		}
-	};
+
+		TEST_METHOD(GetLengthTest1)
+		{
+			int length = 0;
+			int input = 16;
+			setLength(input, &length);
+			Assert::AreEqual(16, length);
+		}
+
+		TEST_METHOD(GetLengthTest2)
+		{
+			int length = 1;
+			int input = 24;
+			setLength(input, &length);
+			Assert::AreEqual(24, length);
+		}
+
+		TEST_METHOD(GetLengthTest3)
+		{
+			int length = 8;
+			int input = 74;
+			setLength(input, &length);
+			Assert::AreEqual(74, length);
+		}
+
+		TEST_METHOD(GetWidthTest1)
+		{
+			int width = 0;
+			int input = 16;
+			setLength(input, &width);
+			Assert::AreEqual(16, width);
+		}
+
+		TEST_METHOD(GetWidthTest2)
+		{
+			int width = 1;
+			int input = 28;
+			setLength(input, &width);
+			Assert::AreEqual(27, width);
+		}
+
+		TEST_METHOD(GetWidthTest3)
+		{
+			int width = 8;
+			int input = 95;
+			setLength(input, &width);
+			Assert::AreEqual(95, width);
+		}
+	}
 }
